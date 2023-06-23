9646164cf5faf23326778458a124da4ad847c738
GabrielGQA <gabrielgqa@gmail.com>
2023-06-23 15:10:17 -0300

    feat: configure jwt

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,18 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { TaskModule } from './task/task.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, UserModule, TaskModule, PrismaModule],
+  imports: [
+    AuthModule,
+    UserModule,
+    TaskModule,
+    PrismaModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,15 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
-  constructor() {
+  constructor(config: ConfigService) {
     super({
       datasources: {
         db: {
-          url: 'postgresql://postgres:123456@localhost:5434/taskdb?schema=public',
+          url: config.get('DATABASE_URL'),
         },
       },
     });
+    console.log(config);
   }
 }
